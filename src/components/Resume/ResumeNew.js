7f841c44a791599651ed6bf27456259f45abd6f6
Flora Dedvukaj <floradedvukaj@Floras-MacBook-Pro.local>
2024-11-04 09:11:33 -0500

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 // import Particle from "../Particle";
-import pdf from "../../Assets/DedvukajFloraResume0926_copy.pdf";
+import pdf from "../../Assets/DedvukajFloraResume1031.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
